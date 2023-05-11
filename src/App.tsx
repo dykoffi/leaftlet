4629e1599e019f659cdd8b3c3bde05b7febf28b8
dykoffi <dykoffi2000@gmail.com>
2023-05-11 16:06:48 +0000

    Add marker icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,15 @@
 import { MapContainer, Popup, TileLayer, GeoJSON, LayersControl, FeatureGroup, Polyline, Marker, Tooltip } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
-import geoFeatures from './data/geoFeatures'
 import gtfs from './data/gtfs'
 import { icon } from 'leaflet'
-import { Grid, Input, Stack, Text, TextInput, Timeline } from '@mantine/core'
+import { Grid, Stack, Text, TextInput, Timeline } from '@mantine/core'
 import { IconCurrentLocation } from "@tabler/icons-react"
 
 function App() {
   const iconDefault = icon({
-
     iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png"
   })
+
   return (
     <Grid gutter={0}>
       <Grid.Col span={3}>
@@ -23,10 +22,10 @@ function App() {
             <Text className='text-gray-500'>SOTRA, Monbus</Text>
           </Stack>
 
-          <Timeline style={{ maxHeight: "100vh" }} active={1} bulletSize={30} lineWidth={10}>
+          <Timeline style={{ maxHeight: "100vh" }} bulletSize={30} lineWidth={10}>
             {
-              gtfs.stops.map((stop) =>
-                <Timeline.Item bullet={<IconCurrentLocation />} title={stop.name}>
+              gtfs.stops.map((stop: any, index: number) =>
+                <Timeline.Item key={index} bullet={<IconCurrentLocation />} title={stop.name}>
                   <Text size="xs" mt={4}>2 hours ago</Text>
                 </Timeline.Item>)
             }
@@ -40,14 +39,14 @@ function App() {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
           {
-            gtfs.stops.map((stop) =>
-              <Marker position={[stop.lat, stop.lon]}>
+            gtfs.stops.map((stop: any, index: number) =>
+              <Marker key={index} position={[stop.lat, stop.lon]}>
                 <Tooltip>
                   {stop.name}
                 </Tooltip>
               </Marker>)
           }
-          <Polyline pathOptions={{ color: "lime", opacity: 0.6, weight: 8 }} positions={gtfs.shapes} />
+          <Polyline pathOptions={{ color: "lime", opacity: 0.6, weight: 15 }} positions={gtfs.shapes} />
         </MapContainer>
       </Grid.Col>
     </Grid>
