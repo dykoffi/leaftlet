e4bbad216ab9c2f260745651d2abfbf30aad179a
dykoffi <dykoffi2000@gmail.com>
2023-05-11 01:01:43 +0000

    Wasm added

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,56 @@
-import { MapContainer, Marker, Popup, TileLayer, GeoJSON, LayersControl, FeatureGroup, LayerGroup, Circle, Rectangle } from 'react-leaflet'
-import geodata from './assets/abidjanGEO.json'
+import { MapContainer, Popup, TileLayer, GeoJSON, LayersControl, FeatureGroup, Polyline, Marker, Tooltip } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import geoFeatures from './data/geoFeatures'
-import { Autocomplete, Select } from '@mantine/core'
-import { GridLayer } from 'leaflet'
-const center = [51.505, -0.09]
-const rectangle = [
-  [51.49, -0.08],
-  [51.5, -0.06],
-]
+import gtfs from './data/gtfs'
+import { icon } from 'leaflet'
+import { Grid, Input, Stack, Text, TextInput, Timeline } from '@mantine/core'
+import { IconCurrentLocation } from "@tabler/icons-react"
+
 function App() {
+  const iconDefault = icon({
+
+    iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png"
+  })
   return (
-    <MapContainer boxZoom style={{ height: "100vh", padding: 0 }} center={[5.37, -4]} zoom={13}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
+    <Grid gutter={0}>
+      <Grid.Col span={3}>
+        <TextInput size='lg' className='' radius={0} placeholder="Rechercher une route" />
+        <Stack p={25} spacing={25}>
+          <Stack spacing={0}>
+            <Text className='text-gray-500' fz={35} fw={800}>
+              Areoport - Gare sud
+            </Text>
+            <Text className='text-gray-500'>SOTRA, Monbus</Text>
+          </Stack>
 
-      <Autocomplete
-        label="Your favorite framework/library"
-        placeholder="Pick one"
-        data={['React', 'Angular', 'Svelte', 'Vue']}
-        />
-      <LayersControl position='bottomright' sortLayers={true}>
-        {
-          geoFeatures.map((feature, index) => {
-            return <LayersControl.Overlay key={index} name={feature.properties.name.toLowerCase()}>
-              <FeatureGroup interactive pathOptions={{ color: 'green', opacity: 0.4, weight: 10 }}>
-                <GeoJSON data={{ type: 'Feature', ...feature }} />
-                <Popup interactive closeButton={false}  >
-                  <p className='font-bold text-xl text-gray-400'>{feature.properties.name}</p>
-                  <div className="text-gray-500">
-                    <b>Operator : </b> {feature.properties.operator} <br />
-                    <b>Network : </b> {feature.properties.network} <br />
-                    <b>Opening Hours : </b> {feature.properties.opening_hours} <br />
-                    <b>Frequency : </b> {feature.properties.frequency} <br />
-                  </div>
-                </Popup>
-              </FeatureGroup>
-            </LayersControl.Overlay>
+          <Timeline style={{ maxHeight: "100vh" }} active={1} bulletSize={30} lineWidth={10}>
+            {
+              gtfs.stops.map((stop) =>
+                <Timeline.Item bullet={<IconCurrentLocation />} title={stop.name}>
+                  <Text size="xs" mt={4}>2 hours ago</Text>
+                </Timeline.Item>)
+            }
 
-          })
-        }
-      </LayersControl>
-    </MapContainer>
+          </Timeline>
+        </Stack>
+      </Grid.Col>
+      <Grid.Col span={9}>
+        <MapContainer boxZoom style={{ height: "100vh", padding: 0 }} center={[5.37, -4]} zoom={13}>
+          <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {
+            gtfs.stops.map((stop) =>
+              <Marker position={[stop.lat, stop.lon]}>
+                <Tooltip>
+                  {stop.name}
+                </Tooltip>
+              </Marker>)
+          }
+          <Polyline pathOptions={{ color: "lime", opacity: 0.6, weight: 8 }} positions={gtfs.shapes} />
+        </MapContainer>
+      </Grid.Col>
+    </Grid>
   )
 
 }
